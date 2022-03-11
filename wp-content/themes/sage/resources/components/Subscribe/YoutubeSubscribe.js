252efc585f2322d3ea74935a8d9034065a3bbc6d
dexidev <dddexadgat@gmail.com>
2022-03-12 01:37:27 +0800

    youtube removed

--- wp-content/themes/sage/resources/components/Subscribe/YoutubeSubscribe.js
+++ wp-content/themes/sage/resources/components/Subscribe/YoutubeSubscribe.js
@@ -7,7 +7,7 @@ const YoutubeSubscribe = () => {
 
     return ( 
         <div className="bg__yt__sub container-fluid blue-gradient py-5" style={{backgroundImage : `url(${SAGE_DIST_PATH + '/assets/images/subscribe.png'})`}}>
-            <div className="container py-5">
+            <div className="container py-5 d-none">
                 <p className="text-center h4 text-white">Stay in the know</p>
                 <p className="text-center h1 text-white">Subscribe to our Youtube channel!</p>
                 <div className="text-center">
