929206e7e6e772e0f82fff5079c2ed8a01e1b387
dexidev <dddexadgat@gmail.com>
2022-03-12 01:23:23 +0800

    utility text ammended

--- wp-content/themes/sage/resources/components/RoadMap/RoadMap.js
+++ wp-content/themes/sage/resources/components/RoadMap/RoadMap.js
@@ -9,7 +9,7 @@ const RoadMap = () => {
                     <div className="row justify-content-center">
                         <div className="col-md-7 col-lg-9">
                             <div className="bg-gray radius p-3 p-md-4">
-                                <p className="text-center h1 blue">ROADMAP</p>
+                                <p className="text-center h1 blue">UTILITY</p>
                                 <p className="text-center text-white mt-4">
                                 Free mint for the next 2 Collings Universe NFT collections
 We currently have plans to launch many NFT collections that Collings Universe holders will get 2 FREE mints to pick any collection of their choice for each NFT they hold. 
