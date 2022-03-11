8572fc1d2692de302d2e62210bb74480fac5b53a
dexidev <dddexadgat@gmail.com>
2022-03-12 01:14:43 +0800

    remove keypass text

--- wp-content/themes/sage/resources/components/KeyPass.js
+++ wp-content/themes/sage/resources/components/KeyPass.js
@@ -11,8 +11,6 @@ const KeyPass = () => {
                     <div className="col-md-6 text-center">
                         <img src={SAGE_DIST_PATH + '/assets/images/nft-thumbnail.png' } className="img-fluid raduis" />
                         <p className="intro__content__maintext text-center text-white">
-                            We currently have 2 additional NFT sets in the works that JRNY NFT CLUB holders will get 1 <br />
-                            FREE mint for each NFT they hold. The first set to come will have amazing art, be part of an epic story line and have its own utility.
                         </p>
                     </div>
                 </div>
