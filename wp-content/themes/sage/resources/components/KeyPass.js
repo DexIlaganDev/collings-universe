b075a295654737db09080a6c360c73e1d8102501
dexidev <dddexadgat@gmail.com>
2022-03-16 03:38:48 +0800

    remove image and added keycard video

--- wp-content/themes/sage/resources/components/KeyPass.js
+++ wp-content/themes/sage/resources/components/KeyPass.js
@@ -9,7 +9,10 @@ const KeyPass = () => {
             <div className="container-fluid">
                 <div className="row justify-content-center">
                     <div className="col-md-6 text-center">
-                        <img src={SAGE_DIST_PATH + '/assets/images/nft-thumbnail.png' } className="img-fluid raduis" />
+
+                        <video autoPlay={true} loop muted className="img-fluid raduis" >
+                            <source src={SAGE_DIST_PATH + '/assets/images/card-key.mp4' } type="video/mp4" />
+                        </video>
                         <p className="intro__content__maintext text-center text-white">
                         </p>
                     </div>
