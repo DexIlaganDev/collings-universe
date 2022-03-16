091eb1b711f27fd01bb4f0afed50084bb882b9c1
dexidev <dddexadgat@gmail.com>
2022-03-17 05:02:22 +0800

    added keypass sections page

--- wp-content/themes/sage/resources/assets/scripts/routes/keypass.js
+++ wp-content/themes/sage/resources/assets/scripts/routes/keypass.js
@@ -4,13 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../../../components/Header';
 import KeyPass from '../../../components/KeyPass';
+import KeyPassPage from '../../../components/KeypassPage/KeyPassPage';
 
 const App = () => {
   return (
       <div>
         <Header />
-        
-        <KeyPass />
+        <KeyPassPage />
       </div>
   )
 }
---
+++ wp-content/themes/sage/resources/components/KeypassPage/KeyPassPage.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const KeyPassPage = () => {
+    return ( 
+        <section id="keypass-page">
+            <div className="container">
+                <div className="row">
+                    <div className="keypass__wrapper col">
+
+                        <section className="keypass__section">
+                            
+                            <header className="keypass__section__header">
+                                <span className='text-white font-avenir-medium'>COLLINGS UNIVERSE</span>
+                                <div className='text-white font-avenir-medium'>KEYPASS</div>
+                            </header>
+
+                            <div className="keypass__section-mint-left">
+                                <span className='text-blue font-hn-light'>0 / 3333 left</span>
+                            </div>
+
+                            <button className="btn btn-connect text-white">CONNECT</button>
+
+                            <div className="keypass__section-mint-max text-white font-hn-light">1 MINT MAX (.1111 ETH)</div>
+
+                        </section>
+
+                        
+                        <section className="keypass__gif">
+                            <div className="keypass__giff__wrapper">
+                                
+                            </div>
+                        </section>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+     );
+}
+ 
+export default KeyPassPage;
