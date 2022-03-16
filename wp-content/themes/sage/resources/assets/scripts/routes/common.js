977e4644f9a80f6fd779375fa16128845b2b21b2
dexidev <dddexadgat@gmail.com>
2022-03-17 03:20:43 +0800

    added keypass routes and moved common and home routes switch

--- wp-content/themes/sage/resources/assets/scripts/routes/common.js
+++ wp-content/themes/sage/resources/assets/scripts/routes/common.js
@@ -1,39 +1,8 @@
-/* eslint-disable */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from '../../../components/Header';
-import IntroContent from '../../../components/IntroContent/IntroContent';
-import KeyPass from '../../../components/KeyPass';
-import RoadMap from '../../../components/RoadMap/RoadMap';
-import Timeline from '../../../components/Timeline/Timeline';
-import FAQ from '../../../components/FAQ/FAQ';
-import Team from '../../../components/Team/Team';
-import YoutubeSubscribe from '../../../components/Subscribe/YoutubeSubscribe';
-
-const App = () => {
-  return (
-      <div>
-        <Header />
-        <IntroContent />
-        <KeyPass />
-        <RoadMap />
-        <Timeline />
-        <FAQ />
-        <Team />
-        <YoutubeSubscribe />
-      </div>
-  )
-}
-
-
-
-
 export default {
   init() {
-    // JavaScript to be fired on all pages
-    ReactDOM.render(<App />, document.getElementById('root'))
+    // JavaScript to be fired on the home page
   },
   finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
+    // JavaScript to be fired on the home page, after the init JS
   },
 };
---
+++ wp-content/themes/sage/resources/assets/scripts/routes/keypass.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../../../components/Header';
+import KeyPass from '../../../components/KeyPass';
+
+const App = () => {
+  return (
+      <div>
+        <Header />
+        <KeyPass />
+      </div>
+  )
+}
+
+export default {
+    init() {
+      ReactDOM.render(<App />, document.getElementById('root-keypass'))
+    },
+  };
+  
