deaab396e92eb74f65db10fb8df78f5e37380ae6
dexidev <dddexadgat@gmail.com>
2022-03-29 08:53:22 +0800

    rename background and added bg images

--- wp-content/themes/sage/resources/components/FAQ/FAQItem.js
+++ wp-content/themes/sage/resources/components/FAQ/FAQItem.js
@@ -4,7 +4,7 @@ const FAQItem = ({question,answer}) => {
     return ( 
         <div className="qa">
             <p>{question}</p>
-            <p class="mb-3 mb-md-4">{answer}</p>
+            <p className="mb-3 mb-md-4">{answer}</p>
         </div>
      );
 }
